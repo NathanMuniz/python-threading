786ddc3eac9a39b796771c4489ede0e327471154
NathanMuniz <natangabriel.palmeiras@gmail.com>
2023-01-25 14:36:27 -0300

    Organizando exemplos

---
+++ ex1.py
@@ -0,0 +1,11 @@
+from time import perf_counter
+
+incio = perf_counter()
+
+
+
+
+
+pausa = perf_counter()
+
+final = inicio - pausa
---
+++ exemplos/ex1.py
@@ -0,0 +1,11 @@
+from time import perf_counter
+
+inicio = perf_counter()
+
+
+
+
+
+pausa = perf_counter()
+
+final_time = inicio - pausa
