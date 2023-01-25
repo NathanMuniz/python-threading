c3dda8dbaeeeb33416985afd4d5e273d57b609f7
NathanMuniz <natangabriel.palmeiras@gmail.com>
2023-01-25 18:14:18 -0300

    ex1.py finished

--- ex1.py
+++ ex1.py
@@ -1,19 +1,20 @@
-from time import perf_counter
-
-incio = perf_counter()
+from time import perf_counter, sleep
 
 
+inicio = perf_counter()
 
+
+def dormindo_por_seg(segundos):
+    print(f'Dormindo por {segundos}segs')
+    sleep(segundos)
+    print('Terminou')
+    
+
+for c in range(0, 9):
+    dormindo_por_seg(1)
 
 
 pausa = perf_counter()
 
 final = inicio - pausa
 
-
-
-
-
-
-
-fsdafasdfasdfasdfasdfasdghp_zG
--- exemplos/ex1.py
+++ exemplos/ex1.py
@@ -1,11 +1,31 @@
-from time import perf_counter
+from time import perf_counter, sleep
+import threading
+
 
 inicio = perf_counter()
 
 
+def dormindo_por_seg(segundos):
+    print(f'Dormindo por {segundos}segs')
+    sleep(segundos)
+    print('Terminou')
+    
+
+threads = []
+
+for c in range(0, 10):
+    t = threading.Thread(target=dormindo_por_seg, args=[1])
+    t.start()
+    threads.append(t)
+
+
+for t in threads:
+    t.join()
 
 
 
 pausa = perf_counter()
 
-temp_final = inicio - pausa
+temp_final = pausa - inicio 
+
+print(f'Seu código levou {temp_final} para executar;')
